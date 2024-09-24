c0ede1c0456ad83ba5362dea1e119f055beabcb5
Nicolas Sawaya <sawayanicolas@gmail.com>
2024-09-24 10:06:44 -0700

    code pasted

---
+++ hamlib/hamlib_snippets.py
@@ -0,0 +1,329 @@
+# Snippets of code aiding HamLib project
+
+import numpy as np
+import networkx as nx
+import h5py
+import mat2qubit
+from openfermion import SymbolicOperator, QubitOperator, FermionOperator
+import copy
+
+# The functions below are for "hierarchical" hdf5 files that make use of groups
+# and subgroups to divide the datasets
+
+
+def _parse_hdf5_recursive(func):
+    """Decorator that recursively iterates through HDF5 file and performs
+    some action that can be specified by `func` on the internal and leaf 
+    nodes in the file."""
+    def wrapper(obj, path='/', key=None):
+        if type(obj) in [h5py._hl.group.Group, h5py._hl.files.File]:
+            for ky in obj.keys():
+                func(obj, path, key=ky, leaf=False)
+                wrapper(obj=obj[ky], path=path + ky + '/', key=ky)
+        elif type(obj) == h5py._hl.dataset.Dataset:
+            func(obj, path, key=None, leaf=True)
+    return wrapper
+
+
+def get_hierarchical_hdf5_keys(fname_hdf5):
+    """Get list of full path keys in hdf5 file. (Applicable to any
+    "hierarchical" HamLib hdf5 file)."""
+    all_keys = []
+
+    @_parse_hdf5_recursive
+    def action(obj, path='/', key=None, leaf=False):
+        if leaf is True:
+            all_keys.append(path)
+
+    with h5py.File(fname_hdf5, 'r') as f:
+        action(f['/'])
+
+    return all_keys
+
+
+def print_hdf5_structure(fname_hdf5):
+    """Print the hierarchical structure in the hdf5 file. (Applicable to any
+    "hierarchical" HamLib hdf5 file)"""
+
+    @_parse_hdf5_recursive
+    def action(obj, path='/', key=None, leaf=False):
+        if key is not None:
+            print((path.count('/')-1)*'\t', '-', key, ':', path + key + '/')
+        if leaf:
+            print((path.count('/')-1)*'\t', '[^^DATASET^^]')
+
+    print(f'start of file: {fname_hdf5}\n')
+    with h5py.File(fname_hdf5, 'r') as f:
+        action(f['/'])
+    print(f'\nend of file: {fname_hdf5}\n')
+
+
+# The functions below are for "flat" hdf5 files that store all the datasets 
+
+
+def get_hdf5_keys(fname_hdf5):
+    """Get list of keys in hdf5 file. (Applicable to any "flat" HamLib hdf5
+    file)"""
+
+    with h5py.File(fname_hdf5, 'r') as f:
+        keys = list(f.keys())
+
+    return keys
+
+
+def save_graph_hdf5(G, fname_hdf5, str_key, overwrite=True, grid_pos=None):
+    """Save networkx graph to the appropriate hdf5 file.
+    Note: This function uses 'a'==APPEND mode. This means that if a particular
+            key is already present in the hdf5 file, it will throw an error.
+            Hence if you're running the same code a second time, you will need
+            to delete the hdf5 file first.
+    """
+    es = list(G.edges)
+
+    with h5py.File(fname_hdf5, 'a') as f:
+
+        if str_key in f:
+            if overwrite:
+                del f[str_key]
+                f[str_key] = np.array(es)
+        else:
+            f[str_key] = np.array(es)
+        
+        if grid_pos is None:
+            pass
+        else:
+            # Store dict{nodes: grid positions} as attribute of each graph
+            for k, v in grid_pos.items():
+                f[str_key].attrs[str(k)]=v
+    
+    return
+        # try:
+        #     f[str_key] = np.array(es)
+        # except OSError:
+        #     del f[str_key]
+        #     f[str_key] = np.array(es)
+        
+
+        
+        
+def read_graph_hdf5(fname_hdf5, str_key):
+    """Read networkx graphs from appropriate hdf5 file.
+    Returns a single graph, with specified str_key
+    """
+    with h5py.File(fname_hdf5, 'r') as f:
+        G = nx.Graph(list(np.array(f[str_key])))
+
+    return G
+        
+    
+
+def read_gridpositions_hdf5(fname_hdf5, str_key):
+    """Read grid positions, stored as attribute of each networkx graph from appropriate hdf5 file
+    Returns grid positions of nodes associated with a single graph, with specified str_key
+    """
+    with h5py.File(fname_hdf5, 'r') as f:
+        dataset = f[str_key]
+        gridpositions_dict = dict(dataset.attrs.items()) 
+
+    return gridpositions_dict
+
+
+
+def save_openfermion_hdf5(qubop, fname_hdf5, str_key, overwrite=True):
+    """Save any openfermion operator object to hdf5 file.
+    Intended for QubitOperator and FermionOperator."""
+    with h5py.File(fname_hdf5, 'a', libver='latest') as f:
+
+        if str_key in f:
+            if overwrite:
+                del f[str_key]
+                f[str_key] = str(qubop)
+        else:
+            f[str_key] = str(qubop)
+
+    return 
+
+def read_openfermion_hdf5(fname_hdf5, str_key, optype=QubitOperator):
+    """Read any openfermion operator object from hdf5 file.
+    'optype' is the op class. Can be QubitOperator or FermionOperator.
+    """
+    with h5py.File(fname_hdf5, 'r', libver='latest') as f:
+        op = optype(f[str_key][()].decode("utf-8"))
+
+    return op
+
+
+def save_mat2qubit_hdf5(symbop, fname_hdf5, str_key, overwrite=True):
+    """Save mat2qubit.qSymbOp operator to hdf5 file"""
+    with h5py.File(fname_hdf5, 'a') as f:
+
+        if str_key in f:
+            if overwrite:
+                del f[str_key]
+                f[str_key] = str(symbop)
+        else:
+            f[str_key] = str(symbop)
+
+    return
+
+def read_mat2qubit_hdf5(fname_hdf5, str_key):
+    """Returns mat2qubit.qSymbOp operator from hdf5 file"""
+    with h5py.File(fname_hdf5, 'r') as f:
+        op = mat2qubit.qSymbOp(f[str_key][()].decode("utf-8"))
+
+    return op
+
+    
+def save_clause_list_hdf5(clause_list, fname_hdf5, str_key):
+    """Save clause list to appropriate hdf5 file
+    Expects clause list to be in DIMACS format
+    """
+    dt = h5py.vlen_dtype(np.dtype('int32'))
+    with h5py.File(fname_hdf5, 'a') as f:
+        dset = f.create_dataset(str_key, (len(clause_list), ), dtype=dt)
+        for i, clause in enumerate(clause_list):
+            dset[i] = np.array(clause)
+        #f[str_key] = [np.array(clause) for clause in clause_list]
+
+
+def read_clause_list_hdf5(fname_hdf5, str_key):
+    """read clause list from appropriate hdf5 file
+    
+    Returns clause list in DIMACS format
+    """
+    clause_list = []
+    with h5py.File(fname_hdf5, 'r') as f:
+        for clause in list(np.array(f[str_key])):
+            clause_list.append([v for v in clause])
+
+    return clause_list
+
+def num_terms(op):
+    """Returns number of terms in object"""
+
+    if isinstance(op, QubitOperator):
+        return len(op.terms)
+    elif isinstance(op, FermionOperator):
+        return len(op.terms)
+    elif isinstance(op, mat2qubit.qSymbOp):
+        return len(op.fullOperator)
+    else:
+        raise TypeError("Object is not a supported operator type.")
+
+
+def getPauliStringLengths(qubop):
+    """Return list of Pauli string lengths in op"""
+    return [len(term) for term in qubop.terms.keys()]
+
+
+def pweight_distr(qubop):
+    """Returns probability weight distribution of terms in openfermion op"""
+
+    if isinstance(qubop, SymbolicOperator):
+
+        # Get histogram of pweights
+        pweight_list_all = getPauliStringLengths(qubop)
+        bins = np.arange( 0, max(pweight_list_all)+2, 1,dtype=int )
+        hist, bin_edges = np.histogram( pweight_list_all, bins=bins, density=False )
+        return dict(zip(bin_edges, hist))
+
+    else:
+        raise TypeError(f"Type {type(qubop)} is not a supported operator type.")
+
+def remove_qindices(op, inds_to_remove):
+    """Removes indices (i.e. removes qubits or fermionic modes) from operator. Any term
+    containing the given indices are removed.
+    
+    Args:
+        op (inhereting from SymbolicOperator): Operator with indices to be removed
+    
+    Returns:
+        new_op (inhereting from SymbolicOperator): New operator with indices removed
+    """
+    
+    assert isinstance(op, SymbolicOperator)
+    # assert inds_to_remove is iterable
+    assert hasattr(inds_to_remove, '__iter__'), "Input is not iterable"
+
+    new_op = copy.deepcopy(op)
+    
+    for term in op.terms:
+        for factor in term:
+            if factor[0] in inds_to_remove:
+                new_op.terms.pop(term)
+                break
+
+    return new_op
+
+
+    
+def filter_dict():
+    raise NotImplementedError()
+    """
+    key_to_object
+    # key_to_dict = lambda key: dict([ a.split('-') for a in key.split('_')[1:]  ])
+    subdict = {k:v for k,v in key_to_object.items() if v['Lx']=='4'}
+    """
+
+def process_keystring(inpkey):
+    """Process keystring into dictionary
+    
+    HamLib format is: 
+    {problemclass}_{varname1}-{var1}_{varnam2}-{var2}…,
+        where ‘problem class’ may include hyphens, but ‘varX’ may not.
+    """
+
+    key_to_dict = {}
+    pieces = inpkey.split('_')
+    key_to_dict['probclass'] = pieces[0]
+    for piece in pieces[1:]:
+        spl = piece.split('-')
+        val = spl[1]
+        if val.isnumeric():
+            key_to_dict[spl[0]] = float(val)
+        else:
+            key_to_dict[spl[0]] = val
+        
+
+    return key_to_dict
+
+def convert_to_dimacs(clause_list, clause_signs):
+    dimacs_list = []
+    for clause, signs in zip(clause_list, clause_signs):
+        dimacs_list.append(tuple([var + 1 if sign == 0 else -(var + 1) for var, sign in zip(clause, signs)]))
+
+    return dimacs_list
+
+def convert_to_hamlib(dimacs_list):
+    clause_list = []
+    clause_signs = []
+    for d_clause in dimacs_list:
+        clause = []
+        signs = []
+        for var in d_clause:
+            clause.append(abs(var) - 1)
+            signs.append(0 if var >0 else 1)
+        clause_list.append(clause)
+        clause_signs.append(signs)
+
+    return clause_list, clause_signs
+
+def remove_smaller_values(op,thresh):
+    """Removes absolute values below given threshold"""
+
+    assert isinstance(op, SymbolicOperator)
+
+    new_op = copy.deepcopy( op )
+
+    for term in op.terms:
+        if abs(op.terms[term]) < thresh:
+            new_op.terms.pop(term)
+    
+    return new_op
+
+
+
+
+
+
+
---
+++ hamlib/test_hamlib_snippets.py
@@ -0,0 +1,321 @@
+# Unit tests for hamlib_snippets
+# 
+# USAGE: pytest -s test_hamlib_snippets.py
+
+from . import hamlib_snippets
+import pytest
+import networkx as nwx
+
+from openfermion import QubitOperator, FermionOperator, count_qubits
+#from mat2qubit import qSymbOp
+
+import os
+
+fname = "test_test_hamlib_snip.hdf5"
+
+
+def test_io_hierarchical_hdf5():
+    # Define
+    op1 = QubitOperator("1 [] + 2 [X0 X2] + 3 [Z0] + 4 [Y1]")
+    op2 = QubitOperator("2 [] + 4 [X0 X2] + 6 [Z0] + 8 [Y1]")
+
+    # Save at hierarchical paths
+    strkey1 = "/toplevel/midlevel/lowlevel/op1"
+    hamlib_snippets.save_openfermion_hdf5(op1, fname, strkey1)
+    strkey2 = "/toplevel/midlevel/op2"
+    hamlib_snippets.save_openfermion_hdf5(op2, fname, strkey2)
+
+    # Print structure of file keys
+    hamlib_snippets.print_hierarchical_hdf5_structure(fname)
+
+    # Get flat list of keys
+    keys = hamlib_snippets.get_hierarchical_hdf5_keys(fname)
+
+    # Load
+    op_loaded1 = hamlib_snippets.read_openfermion_hdf5(fname, keys[0])
+    op_loaded2 = hamlib_snippets.read_openfermion_hdf5(fname, keys[1])
+
+    # Compare
+    assert len(keys) == 2
+    assert op1 == op_loaded1
+    assert op2 == op_loaded2
+
+    # Delete file
+    os.remove(fname)
+
+
+def test_io_qubitoperator():
+    
+    # Define
+    op = QubitOperator("1 [] + 2 [X0 X2] + 3 [Z0] + 4 [Y1]")
+
+    # Save
+    strkey = "testkey"
+    hamlib_snippets.save_openfermion_hdf5(op,fname,strkey)
+    # Save again to test duplicate-saving
+    hamlib_snippets.save_openfermion_hdf5(op,fname,strkey)
+
+    # Load
+    op_loaded = hamlib_snippets.read_openfermion_hdf5(fname,strkey)
+
+    # Compare
+    assert op == op_loaded
+
+    # Delete file
+    os.remove(fname)
+
+
+def test_io_fermionoperator():
+
+    # Define
+    op = FermionOperator("1 [] + 2 [0^ 2] + 3 [0^ 0] + 4 [4^ 3 9 3^]")
+
+    # Save
+    strkey = "testkey"
+    hamlib_snippets.save_openfermion_hdf5(op,fname,strkey)
+    # Save again to test duplicate-saving
+    hamlib_snippets.save_openfermion_hdf5(op,fname,strkey)
+
+    # Load
+    operator_class = FermionOperator
+    op_loaded = hamlib_snippets.read_openfermion_hdf5(fname,strkey,operator_class)
+
+    # Compare
+    assert op == op_loaded
+
+    # Delete file
+    os.remove(fname)
+
+
+"""
+def test_io_mat2qubit():
+    
+    # Define
+    op = qSymbOp(" k [ n_A ] ++ j [ p_B ] ++ r [] ")
+    # Save
+    strkey = "testkey"
+    hamlib_snippets.save_mat2qubit_hdf5(op,fname,strkey)
+    # Save again to test duplicate-saving
+    hamlib_snippets.save_mat2qubit_hdf5(op,fname,strkey)
+    # Load
+    op_loaded = hamlib_snippets.read_mat2qubit_hdf5(fname,strkey)
+    # Compare
+    assert str(op) == str(op_loaded)
+    # Delete file
+    os.remove(fname)
+"""
+
+
+
+def test_io_graph():
+
+    # create graph G 
+    G_grid = nwx.grid_graph(dim=(3,))
+    G = nwx.convert_node_labels_to_integers(G_grid)
+
+    # save graph G 
+    strkey = "testkey"
+    hamlib_snippets.save_graph_hdf5(G, fname, strkey)
+
+    # get the keys and confirm correct
+    keys = hamlib_snippets.get_hdf5_keys(fname)
+    assert len(keys) == 1
+    assert strkey == keys[0]
+
+    # read graph and confirm correct 
+    G_out = hamlib_snippets.read_graph_hdf5(fname, strkey)
+    assert nwx.is_isomorphic(G_out, G)
+
+    # attempt to read grid_pos from hdf5 file and confirm there is no grid_pos attribute
+    grid_pos_out = hamlib_snippets.read_gridpositions_hdf5(fname, strkey)
+    assert grid_pos_out == {}
+
+    # delete file
+    os.remove(fname)
+    
+    
+    
+def test_io_graph_wgridpos():
+
+    # create graph G and get grid positions grid_pos
+    G_grid = nwx.grid_graph(dim=(3,))
+    G = nwx.convert_node_labels_to_integers(G_grid)
+    nodes = list(G.nodes)
+    gridnodes = list(G_grid.nodes)
+    grid_pos = {str(nodes[i]):gridnodes[i] for i in range(0,3)}
+
+    # save graph G and save grid_pos dict as attribute
+    strkey = "testkey"
+    hamlib_snippets.save_graph_hdf5(G, fname, strkey, grid_pos=grid_pos)
+
+    # get the keys and confirm correct
+    keys = hamlib_snippets.get_hdf5_keys(fname)
+    assert len(keys) == 1
+    assert strkey == keys[0]
+
+    # read graph and confirm correct 
+    G_out = hamlib_snippets.read_graph_hdf5(fname, strkey)
+    assert nwx.is_isomorphic(G_out, G)
+
+    # read grid_pos and confirm correct
+    grid_pos_out = hamlib_snippets.read_gridpositions_hdf5(fname, strkey)
+    assert grid_pos_out == grid_pos
+
+    # delete files
+    os.remove(fname)
+
+    
+    
+def test_cleanup():
+    # Clean up--will be required if any of above tests fail
+    if os.path.exists(fname):
+        os.remove(fname)
+
+
+def test_num_terms():
+    # Test number of terms
+    op = QubitOperator("1 [] + 2 [X0 X2] + 3 [Z0] + 4 [Y1]")
+    assert hamlib_snippets.num_terms(op) == 4
+
+    op = FermionOperator("1 [] + 2 [0^ 2] + 3 [0^ 0] + 4 [4^ 3 9 3^]")
+    assert hamlib_snippets.num_terms(op) == 4
+
+    #op = qSymbOp(" k [ n_A ] ++ j [ p_B ] ++ r [] ")
+    #assert hamlib_snippets.num_terms(op) == 3
+
+
+def test_pweight_distr():
+    # Test pweight_distr
+    op = QubitOperator("1 [] + 2 [X0 X2] + 3 [Z0] + 4 [Y1]")
+    assert hamlib_snippets.pweight_distr(op) == {0: 1, 2: 1, 1: 2}
+
+
+def test_process_keystring():
+    # Test process_keystring()
+    keystr = "graph-2D-triag-pbc-qubitnodes_Lx-4_Ly-198"
+    gold = {'probclass': 'graph-2D-triag-pbc-qubitnodes', 'Lx': 4, 'Ly': 198}
+    res = hamlib_snippets.process_keystring(keystr)
+    assert res == gold
+
+
+def test_clause_list():
+    # Example clause list with sign list
+    clause_list = [
+            (1,2,3),
+            (2,4,5),
+            (1,3,6),
+            (2,3,4)
+            ]
+
+    sign_list = [
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 1),
+            (0, 1, 1),
+            ]
+
+    # Save clause+sign list in dimacs form
+    dimacs_format = hamlib_snippets.convert_to_dimacs(clause_list, sign_list)
+    hamlib_snippets.save_clause_list_hdf5(dimacs_format, "test_clause_list_snippets.hdf5", "example_clause")
+
+    # Read in clause list (dimacs form)
+    test_clause_list = hamlib_snippets.read_clause_list_hdf5("test_clause_list_snippets.hdf5", "example_clause")
+
+    # Convert to sets for easy checking
+    base_clause_set = set([tuple(c) for c in dimacs_format])
+    test_clause_set = set([tuple(c) for c in test_clause_list])
+
+    # Check if the clause lists are the same
+    assert base_clause_set == test_clause_set
+
+
+def test_convert_to_dimacs():
+    # Example clause list with sign list
+    clause_list = [
+            (1,2,3),
+            (2,4,5),
+            (1,3,6),
+            (2,3,4)
+            ]
+
+    sign_list = [
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 1),
+            (0, 1, 1),
+            ]
+
+    # Save clause+sign list in dimacs form
+    dimacs_format = hamlib_snippets.convert_to_dimacs(clause_list, sign_list)
+
+    expected = set()
+    expected.add((1, 2, -3))
+    expected.add((2, -4, 5))
+    expected.add((-1, 3, -6))
+    expected.add((2, -3, -4))
+
+    dimacs_set = set([c for c in dimacs_format])
+
+    assert expected == dimacs_set
+
+
+def test_convert_from_dimacs():
+    dimacs = [
+        (1, 2, -3),
+        (2, -4, 5),
+        (-1, 3, -6),
+        (2, -3, -4),
+        ]
+
+    expected_clause_set = set([
+            (1,2,3),
+            (2,4,5),
+            (1,3,6),
+            (2,3,4)
+            ])
+
+    expected_sign_set = set([
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 1),
+            (0, 1, 1),
+            ])
+
+    converted_clause_list, converted_sign_list = hamlib_snippets.convert_to_hamlib(dimacs)
+
+    converted_clause_set = set([tuple(c) for c in converted_clause_list])
+    converted_sign_set = set([tuple(s) for s in converted_sign_list])
+
+    assert expected_clause_set == converted_clause_set
+    assert expected_sign_set == converted_sign_set
+
+
+def test_remove_qindices():
+
+    qop  = QubitOperator("-1 [] + -2 [X0 X2] + -3 [Z0] + -4 [Y1]")
+    res  = hamlib_snippets.remove_qindices(qop,(2,))
+    gold = QubitOperator("-1 [] + -3 [Z0] + -4 [Y1]")
+    assert gold==res, (gold,res)
+    assert 2 == count_qubits(gold)
+
+    res = hamlib_snippets.remove_qindices(qop,(1,))
+    gold = QubitOperator("-1 [] + -2 [X0 X2] + -3 [Z0]")
+    assert gold==res, (gold,res)
+    assert 3 == count_qubits(gold)
+
+
+
+
+def test_remove_smaller_values():
+    # Test remove_smaller_values
+    op = QubitOperator("-1 [] + -2 [X0 X2] + -3 [Z0] + -4 [Y1]")
+    hamlib_snippets.remove_smaller_values(op, 3)
+    assert op == QubitOperator("3 [Z0] + 4 [Y1]")
+
+
+
+
+
+
+
+
